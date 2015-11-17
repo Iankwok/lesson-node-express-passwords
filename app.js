0cd769b881bf7cb340a75ec8b3283160bf58faec
“Ian <ianyhkwok@hotmail.com>
2015-11-17 12:22:56 +0800

    User Sign up and login done

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
 mongoose.connect('mongodb://localhost:27017/authentication-practise');
 
 // Only render errors in development
@@ -24,5 +26,46 @@ if (app.get('env') === 'development') {
     });
   });
 }
+
+app.post("/signup", function(req, res) {
+  var userObject = new User(req.body.user);
+
+  if(!userParams.email)
+    {return res.status(401).send({message: "Missing Email"});}
+
+  if(!userParams.password)
+    {return res.status(401).send({message: "Missing Password"});}
+
+  if(!userParams.password !== userParams.passwordConfirmation)
+    {return res.status(401).send({message: "Password not match"});}
+
+  var userObject = new User(userParams);
+  userObject.save(function(err, user) {
+    if(err){
+      return res.status(401).send({message: err.errmsg});
+    } else {
+      return res.status(200).send({message: "user created"});
+    }
+  });
+})
+
+app.post("/signin", function(req, res) {
+  var userParams = req.body.user;
+
+  User.findOne({ email: userParams.email }, function(err, user) {
+    if(!user){return res.status(404).send({message: "No user found"});}
+
+    user.authenticate(userParams.password, function(err, isMatch) {
+      if (err) throw err;
+
+      if (isMatch) {
+        return res.status(200).send({message: "Valid Credentials !"});
+      } else {
+        return res.status(401).send({message: "The credentials provided do not correspond to a registered user"});
+      };
+    });
+  });
+});
+
 
 app.listen(3000);
